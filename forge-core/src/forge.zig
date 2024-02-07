36d0f4948af27a6bc56d071a2adf26e9e804b69d
griush <gerardriush@gmail.com>
2024-02-07 14:51:01 +0100

    Implemented basic engine-client structure...
    ... and improved logger

--- forge-core/src/forge.zig
+++ forge-core/src/forge.zig
@@ -1,10 +1,11 @@
 // Cient usage file. All exposed structs, functions and constants will be accessed form here
-
-pub const logger = @import("logger/logger.zig");
+pub const application = @import("core/application.zig");
+pub const engine = @import("core/engine.zig");
+pub const logger = @import("core/logger.zig");
 
 const forge_version_name: []const u8 = "0.0.1-alpha";
 
-/// Returns: string the current core version. 
+/// Returns: (string) the current core version.
 pub fn getVersionName() []const u8 {
     return forge_version_name;
 }
---
+++ forge-runtime/src/runtime.zig
@@ -0,0 +1,10 @@
+const forge = @import("forge");
+
+pub fn onInit() void {
+}
+
+pub fn onUpdate(delta_time: f64) void {
+    // this is here so compiler does not complain about unused parameter
+    // delete or uncomment when using deltaTime
+    _ = delta_time;
+}
