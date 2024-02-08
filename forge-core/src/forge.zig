9fbb9ecfae268c9b798e67207d472b1861f0e45a
griush <gerardriush@gmail.com>
2024-02-09 00:02:17 +0100

    Added missing commit files
    I am stupid.

--- forge-core/src/forge.zig
+++ forge-core/src/forge.zig
@@ -1,8 +1,12 @@
 // Cient usage file. All exposed structs, functions and constants will be accessed form here
+// Core
 pub const application = @import("core/application.zig");
 pub const engine = @import("core/engine.zig");
 pub const logger = @import("core/logger.zig");
 pub const events = @import("core/events.zig");
+
+// Math
+pub const math = @import("math/math.zig");
 
 const forge_version_name: []const u8 = "0.0.1-alpha";
 
--- forge-runtime/src/runtime.zig
+++ forge-runtime/src/runtime.zig
@@ -1,12 +1,19 @@
 const forge = @import("forge");
-
-fn onKeyPressEvent(payload: forge.events.EventPayload) bool {
-    forge.logger.trace("Key Code: {d}", .{payload.key_code});
-    return false;
-}
+const math = forge.math;
 
 pub fn onInit() void {
-    forge.events.registerEvent(forge.events.EventType.KeyPress, onKeyPressEvent);
+    var v1 = math.Vec3.init(1.0, 2.5, 1.0);
+    const v2 = v1.add(math.Vec3{
+        .data = [_]f32 {
+            0.5, 1.5, -1.0,
+        }
+    });
+
+    const v3 = v1.normalize();
+
+    forge.logger.debug("v1: {s}", .{v1});
+    forge.logger.debug("v1 + (0.5, 1.5): {s}", .{v2});
+    forge.logger.debug("v1 normalized: {s}", .{v3});
 }
 
 pub fn onUpdate(delta_time: f64) void {
