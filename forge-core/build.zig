9a69de6142d3bb4578442be87a4d9a316503f5de
griush <gerardriush@gmail.com>
2024-02-09 14:35:30 +0100

    Update zig version

--- forge-core/build.zig
+++ forge-core/build.zig
@@ -1,10 +1,18 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) !void {
-    // const target = b.standardTargetOptions(.{});
-    // const optimize = b.standardOptimizeOption(.{});
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    const forge_core_module = b.createModule(.{ .source_file = .{ .path = "src/forge.zig" } });
+    const forge_mod = b.addModule("forge", .{  .root_source_file = .{ .path = "src/forge.zig" }, });
+    const forge_lib = b.addStaticLibrary(.{
+        .name = "forge",
+        .root_source_file = .{ .path = "src/forge.zig" },
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
 
-    try b.modules.put(b.dupe("forge"), forge_core_module);
+    b.installArtifact(forge_lib);
+    forge_mod.linkLibrary(forge_lib);    
 }
