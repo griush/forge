dadaca183f7ed4c07610f97d9b9ab78bca9ef84c
griush <gerardriush@gmail.com>
2024-02-11 22:43:33 +0100

    Fixed working_dir default value

--- forge-core/src/core/application.zig
+++ forge-core/src/core/application.zig
@@ -2,7 +2,7 @@ const events = @import("events.zig");
 
 pub const Application = struct {
     name: []const u8,
-    working_dir: []const u8 = "",
+    working_dir: []const u8 = ".",
 
     on_init: *const fn () void,
     on_update: *const fn (f64) void,
