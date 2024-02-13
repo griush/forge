0196611a0cee8f3a902e386ad0bffd0b6090150b
Gerard Rius <gerardriush@gmail.com>
2024-02-13 15:55:04 +0100

    Fix for linux

--- forge-core/build.zig
+++ forge-core/build.zig
@@ -19,19 +19,19 @@ pub fn build(b: *std.Build) void {
     forge_mod.linkLibrary(glfw_dep.artifact("glfw"));
     @import("glfw").addPaths(forge_mod);
 
-    const vulkan_sdk_env = "VK_SDK_PATH";
-    const vulkan_sdk_path = std.process.getEnvVarOwned(b.allocator, vulkan_sdk_env) catch |err| {
-        std.log.err("Error: {s}", .{@errorName(err)});
-        @panic("Vulkan SDK not found.");
-    };
-
-    forge_mod.addLibraryPath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/Lib", .{vulkan_sdk_path}) catch @panic("OOM") });
-    forge_mod.addIncludePath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/Include", .{vulkan_sdk_path}) catch @panic("OOM") });
-
     if (target.result.os.tag == .windows) {
         forge_mod.linkSystemLibrary("gdi32", .{});
         forge_mod.linkSystemLibrary("user32", .{});
         forge_mod.linkSystemLibrary("shell32", .{});
+
+        const vulkan_sdk_env = "VK_SDK_PATH";
+        const vulkan_sdk_path = std.process.getEnvVarOwned(b.allocator, vulkan_sdk_env) catch |err| {
+            std.log.err("Error: {s}", .{@errorName(err)});
+            @panic("Vulkan SDK not found.");
+        };
+
+        forge_mod.addLibraryPath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/Lib", .{vulkan_sdk_path}) catch @panic("OOM") });
+        forge_mod.addIncludePath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/Include", .{vulkan_sdk_path}) catch @panic("OOM") });
 
         forge_mod.linkSystemLibrary("vulkan-1", .{
             .preferred_link_mode = .Static,
